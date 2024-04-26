81b6343cae930c99bc6808d7ec58fc4d0cec4ca2
titustasneem <97880296+titustasneem@users.noreply.github.com>
2024-04-26 14:12:32 +0200

    added about section

--- sa-coffee-company/src/App.js
+++ sa-coffee-company/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css'
-// import About from './components/About'
+import About from './components/About'
 // import Book from './components/Book'
 // import Contact from './components/Contact'
 // import Footer from './components/Footer'
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Home/>
-      {/* <About/> */}
+      <About/>
       {/* <Book/> */}
       {/* <Contact/> */}
       {/* <Footer/> */}
---
+++ sa-coffee-company/src/components/About.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='aka' >
+        <section className="about" id="about">
+            <h1 className="heading">about us</h1>
+            <div className="row">
+                <div className="image">
+                    <img src={"../assets/images/home-img-secondary.png"} alt="" />
+                </div>
+                <div className="content">
+                  <h3 className="title">Why Choose Us</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id perferendis dolores, atque tenetur laboriosam error!</p>
+                  <a href="#" className='btn' >Read more</a>
+                  <div className="icons-container">
+                    <div className="icons">
+                      <img src={"../assets/images/home-img-secondary.png"} alt="" />
+                      <h3>Reason 3</h3>
+                    </div>
+                    <div className="icons">
+                      <img src={"../assets/images/home-img-secondary.png"} alt="" />
+                      <h3>Reason 2</h3>
+                    </div>
+                    <div className="icons">
+                      <img src={"../assets/images/home-img-secondary.png"} alt="" />
+                      <h3>Reason 1</h3>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default About
